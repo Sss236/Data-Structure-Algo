12e1a3b86fbedd62adce6444a1e4f50a8c96a148
Yaqian Yang <yang.yaqi@northeastern.edu>
2021-06-30 18:47:15 +0800

    done

--- labassignment6/lab6.c
+++ labassignment6/lab6.c
@@ -1,39 +1,68 @@
-//enter your name here
-//enter your email here
+//enter your name here Yaqian Yang
+//enter your email here yang.yaqi@northeastern.edu
 
 #include <stdio.h>
 #include <string.h>
-#include<ctype.h>
+#include <ctype.h>
 
 /* function to encrypt the data*/
 void encrypt(char text[], int key)
 {
     //insert your code here
+    int i;
+    char ch;
+    for (i = 0; text[i] != '\0'; ++i)
+    {
+        ch = text[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = (ch - 'a' + key) % 26 + 'a';
+            text[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = (ch - 'A' + key) % 26 + 'A';
+            text[i] = ch;
+        }
+    }
 }
 
 /*function to decrypt the data*/
-void decrypt(char text[],int key)
+void decrypt(char text[], int key)
 {
-    
+    int i;
+    char ch;
     //insert your code here
-    
+    for (i = 0; text[i] != '\0'; ++i)
+    {
+        ch = text[i];
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = (ch - 'a' - key) % 26 + 'a';
+            text[i] = ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = (ch - 'A' - key) % 26 + 'A';
+            text[i] = ch;
+        }
+    }
 }
-
 
 /*----------- Main program---------------*/
 int main()
 {
-    char text[20] ;
+    char text[20];
     /*Input string */
     printf("Enter the message:  ");
-    scanf("%s",text);
-    printf("text input   = %s\n",text);
+    scanf("%s", text);
+    printf("text input   = %s\n", text);
     /*call function to encrypt the data*/
-    encrypt(text,3);
-    printf("Encrypted value = %s\n",text);
+    encrypt(text, 3);
+    printf("Encrypted value = %s\n", text);
     /*call function to decrypt the data*/
-    decrypt(text,3);
-    printf("Decrypted value = %s\n",text);
-    
+    decrypt(text, 3);
+    printf("Decrypted value = %s\n", text);
+
     return 0;
 }
