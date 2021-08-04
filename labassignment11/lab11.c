e25e33ec9d313cc56adfaa0fadc4b9825784a28a
Yaqian Yang <yang.yaqi@northeastern.edu>
2021-08-04 23:44:11 +0800

    done

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -1,5 +1,5 @@
-//Enter your name here
-//Enter your email here
+//Enter your name here: Yaqian Yang
+//Enter your email here: yang.yaqi@northeastern.edu
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -11,10 +11,18 @@ void *philosopher(void *x)
  //Treat variable x as a pointer to an int and then extract the value into n
     int* a=(int*)x;
     int n=*a;
-    
-        /*-----Insert your code here----*/
-    
-    
+    /*-----Insert your code here----*/
+    int right = n;
+    int left = n - 1;
+    if(left == -1) left = 4;
+    printf("philosopher %d is thinking \n", n + 1);
+    pthread_mutex_lock(&chopstick[left]);
+    pthread_mutex_lock(&chopstick[right]);
+    printf("philosopher %d is eating with [%d] [%d] \n", n + 1, left, right);
+    sleep(1);
+    pthread_mutex_unlock(&chopstick[left]);
+    pthread_mutex_unlock(&chopstick[right]);
+    printf("philosopher %d finishes eating \n", n + 1);
 }
 
 
@@ -25,12 +33,18 @@ int main()
     int i,val[5];
     
 //initializing the mutex
-    
+    for(i = 0; i < 5; i++){
+        pthread_mutex_init( &chopstick[i], NULL);
+    }
 //create and run the thread
-    
+    for(i = 0; i < 5; i++){
+        pthread_create( &threads[i], NULL, philosopher, (void*)&i);
+    }
 //wait until all the threads are done
-    
+    for(i=0; i<5; i++)
+        pthread_join(threads[i],NULL);
 // Destroying the mutex
-    
+    for(i=0; i<5; i++)
+        pthread_mutex_destroy(&chopstick[i]);
     return 0;
 }
